ba6567214ecfa887aa23fe5e0bc1ea232f600867
Tim Jones <tim@timjones.tw>
2014-10-31 01:05:08 +0800

    Add more examples to TestAssembly

---
+++ src/TestAssembly/GenericClass1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestAssembly
+{
+    /// <summary>
+    /// This is a class with generic type parameters.
+    /// </summary>
+    public class GenericClass1<T>
+        where T : IEnumerable<int>
+    {
+        /// <summary>
+        /// This is a method with a generic type parameter.
+        /// </summary>
+        /// <typeparam name="U">The generic type parameter.</typeparam>
+        public Tuple<T, U> GenericMethod<U>()
+        {
+            return null;
+        }
+    }
+}
---
+++ src/TestAssembly/IInterface1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestAssembly
+{
+    /// <summary>
+    /// This is an interface.
+    /// </summary>
+    public interface IInterface1 : IDisposable
+    {
+        /// <summary>
+        /// Interface method.
+        /// </summary>
+        void DoSomething();
+    }
+}
