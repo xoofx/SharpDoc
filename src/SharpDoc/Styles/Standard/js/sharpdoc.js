75d2a5d3ab9c6862fc2d1bb825cc4e903c148570
Robert Rouhani <robert.rouhani@gmail.com>
2015-07-13 00:14:47 -0400

    Updated mootools and added URI to mootools-more.
    Replaced manual parsing of "page" GET variable w/ mootools URI class
    Fixed regex for "_SubTopics" parent id to allow all valid HTML5 ids.
    Refactored "hightLightTopic" to "highlightTopic"

--- src/SharpDoc/Styles/Standard/js/sharpdoc.js
+++ src/SharpDoc/Styles/Standard/js/sharpdoc.js
@@ -44,78 +44,18 @@ function autoResize(id) {
 }
 
 function loadContent(rootTopic, extension) {
-    var data = window.location.search;
     var url = rootTopic + extension;
     var topicToHighlight = rootTopic;
 
-    if(data != null && data !="")
+    var data = new URI().getData("page");
+    if(data)
     {
-        var page = $get("page", data);
-        if(page != "")
-        {
-            url = page + extension;
-            topicToHighlight = page;
-        }
+        url = data + extension;
+        topicToHighlight = data;
     }
     
     $("mainFrame").setAttribute("src", url);
-    hightLightTopic(topicToHighlight);
-}
-
-/*
-  http://webfreak.no/wp/2007/09/05/get-for-mootools-a-way-to-read-get-variables-with-javascript-in-mootools/
-Function: $get
-	This function provides access to the "get" variable scope + the element anchor
-
-Version: 1.3
-
-Arguments:
-	key - string; optional; the parameter key to search for in the url's query string (can also be "#" for the element anchor)
-	url - url; optional; the url to check for "key" in, location.href is default
-
-Example:
-	>$get("foo","http://example.com/?foo=bar"); //returns "bar"
-	>$get("foo"); //returns the value of the "foo" variable if it's present in the current url(location.href)
-	>$get("#","http://example.com/#moo"); //returns "moo"
-	>$get("#"); //returns the element anchor if any, but from the current url (location.href)
-	>$get(,"http://example.com/?foo=bar&bar=foo"); //returns {foo:'bar',bar:'foo'}
-	>$get(,"http://example.com/?foo=bar&bar=foo#moo"); //returns {foo:'bar',bar:'foo',hash:'moo'}
-	>$get(); //returns same as above, but from the current url (location.href)
-	>$get("?"); //returns the query string (without ? and element anchor) from the current url (location.href)
-
-Returns:
-	Returns the value of the variable form the provided key, or an object with the current GET variables plus the element anchor (if any)
-	Returns "" if the variable is not present in the given query string
-
-Credits:
-		Regex from [url=http://www.netlobo.com/url_query_string_javascript.html]http://www.netlobo.com/url_query_string_javascript.html[/url]
-		Function by Jens Anders Bakke, webfreak.no
-*/
-function $get(key, url) {
-    if (arguments.length < 2) url = location.href;
-    if (arguments.length > 0 && key != "") {
-        if (key == "#") {
-            var regex = new RegExp("[#]([^$]*)");
-        } else if (key == "?") {
-            var regex = new RegExp("[?]([^#$]*)");
-        } else {
-            var regex = new RegExp("[?&]" + key + "=([^&#]*)");
-        }
-        var results = regex.exec(url);
-        return (results == null) ? "" : results[1];
-    } else {
-        url = url.split("?");
-        var results = {};
-        if (url.length > 1) {
-            url = url[1].split("#");
-            if (url.length > 1) results["hash"] = url[1];
-            url[0].split("&").each(function (item, index) {
-                item = item.split("=");
-                results[item[0]] = item[1];
-            });
-        }
-        return results;
-    }
+    highlightTopic(topicToHighlight);
 }
 
 
@@ -240,7 +180,7 @@ function openToc(nodeId) {
     }
 
     var parent = node.getParent().id;
-    var pattern = /(\w+)_SubTopics/;
+    var pattern = /([^\s]+)_SubTopics/;
     var parentId = pattern.exec(parent);
     if (parentId != null)
         openToc(parentId[1]);
@@ -276,7 +216,7 @@ function toggleToc(nodeId) {
 
 }
 
-function hightLightTopic(topicId) {
+function highlightTopic(topicId) {
     //alert("highlight");
     var oldHighlight = $$_('.highlight');
     oldHighlight.each(function (old, oldId) {
